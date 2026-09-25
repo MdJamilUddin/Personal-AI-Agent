6fda3ffbafe18a7b42bcc12ee055d85d62b99463
Md. Jamil Uddin <121606336+MdJamilUddin@users.noreply.github.com>
2026-09-25 14:30:53 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "admin-agent-v5";
+const CACHE = "admin-agent-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
